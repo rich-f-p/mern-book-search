fa57230ed36df7e06a9037543c33379fcc94b0f5
rich p <richardferry3@gmail.com>
2022-06-03 21:28:20 -0700

    create apollo provider

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,25 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 import { setContext } from '@apollo/client/link/context'
 
+const httpLink = new ApolloClient({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, {headers}) =>{
+  const token = localStorage.getItem('id_token');
+  return {
+    headers:{...headers, authorization: token ? `Bearer ${token}` : ''}
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -19,6 +35,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
